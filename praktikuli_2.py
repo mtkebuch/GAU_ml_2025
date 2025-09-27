fa59c3fb1eced701171f5863d99ea252fc0d478b
Mariami <mtkebuchava222@gmail.com>
2025-09-27 12:01:02 +0400

    praktikuli 2

---
+++ praktikuli_2.py
@@ -0,0 +1,17 @@
+#2
+list = [2,35,1]
+print(max(list))
+
+#6
+import random
+for _ in range(10):
+    print(random.randint(1,89))
+
+#8
+def average(a,b):
+    return (a+b)/20
+average(4,2)
+average(6,7)
+average(2,1)
+
+#11
